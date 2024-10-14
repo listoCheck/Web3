0073aa3827f505c7370521b6ba47c7a87d645a6e
Artem <v.amuzinsky@yandex.ru>
2024-10-14 17:45:14 +0300

    готово

--- src/main/java/org/example/web3/baze/Test.java
+++ src/main/java/org/example/web3/baze/Test.java
@@ -1,8 +1,8 @@
 package org.example.web3.baze;
 import java.sql.*;
 public class Test {
-    //public static final String DATABASE_HELIOS_URL = "jdbc:postgresql://pg/studs";
-    public static final String DATABASE_HELIOS_URL = "jdbc:postgresql://localhost:5432/studs";
+    public static final String DATABASE_HELIOS_URL = "jdbc:postgresql://pg/studs";
+    //public static final String DATABASE_HELIOS_URL = "jdbc:postgresql://localhost:5432/studs";
     public static Statement statmt;
     public static Connection connection;
     public static void test(){
